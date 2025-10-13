d8ea64dfb14cdd28100c2aac8c9c2ef0641aa24d
Anteneh Mullu <anteneh.mullu@gmail.com>
2025-10-13 22:58:03 +0300

    Day 1:Python Variables

---
+++ Day-1:Variables/band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to Band The Generator.")
+city=input("What is the name of the city you grew up?\n")
+pet=input("What's your pet's name?\n")
+brand_name=city + " " + pet
+print(f"Your Brand name could be Bristol Rabit")

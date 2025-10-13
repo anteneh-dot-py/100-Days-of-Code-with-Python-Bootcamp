9f618b3bf5700cf1ba6f2dd127df4cd0e679f02f
Anteneh Mullu <anteneh.mullu@gmail.com>
2025-10-13 23:08:46 +0300

    Day 1:Python Variables

--- Day-1:Variables/band_name_generator.py
+++ Day-1:Variables/band_name_generator.py
@@ -2,4 +2,9 @@ print("Welcome to Band The Generator.")
 city=input("What is the name of the city you grew up?\n")
 pet=input("What's your pet's name?\n")
 brand_name=city + " " + pet
-print(f"Your Brand name could be {brand_name}")
+
+# solution 1 set the result on brand_name variable
+# print(f"Your Brand name could be {brand_name}")
+
+# solution 2 concatinate the variable using python f string
+print(f"Your Brand name could be {city} {pet}")
